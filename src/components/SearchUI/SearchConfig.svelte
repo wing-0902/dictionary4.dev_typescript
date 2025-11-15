474cbad13160cde77cc00fffa79c79d14c31e165
Wing <114337707+wing-0902@users.noreply.github.com>
2025-11-15 10:33:59 +0000

    input

--- src/components/SearchUI/SearchConfig.svelte
+++ src/components/SearchUI/SearchConfig.svelte
@@ -1,11 +1,20 @@
 <script lang='ts'>
-
+  let searchType: string = 'title';
+  let searchKeyword: string = '';
 </script>
 
 <div>
-
+  <div class='searchSlotRow'>
+    <input type='text' bind:value={searchKeyword} />
+  </div>
 </div>
 
 <style lang='scss'>
-
+  .searchSlotRow {
+    display: flex;
+    input {
+      width: 100%;
+      height: 40px;
+    }
+  }
 </style>
