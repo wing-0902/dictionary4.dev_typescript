3ae3ec5399f1fb6064cbf8b48f8d5f69be55bbda
Wing <114337707+wing-0902@users.noreply.github.com>
2025-11-21 10:25:08 +0000

    こうか？

--- src/components/SearchUI/IslandEntrance.svelte
+++ src/components/SearchUI/IslandEntrance.svelte
@@ -1,11 +1,24 @@
 <script lang='ts'>
   import ModeConfig from "./ModeConfig.svelte";
   import PagefindConnector from "./PagefindConnector.svelte";
+  import TitlePagefind from "./TitlePagefind.svelte";
 
   let searchMode: string = '見出し'
+
+  $: SearchComponent =
+    searchMode === '見出し' ? TitlePagefind :
+    searchMode === '全文' ? PagefindConnector :
+    null;
 </script>
 
 <div class='root'>
+  {#if (SearchComponent !== null)}
+    <svelte:component this={SearchComponent}>
+      <ModeConfig bind:searchMode = {searchMode} />
+    </svelte:component>
+  {:else}
+    <h2>エラーが発生しました．</h2>
+  {/if}
   <PagefindConnector>
     <ModeConfig bind:searchMode={searchMode} />
   </PagefindConnector>
